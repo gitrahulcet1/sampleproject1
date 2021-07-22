e2353cfb6f170186ccc858bd56a9987c983c9ec7
girrahulcet1 <writetorahul96@gmail.com>
2021-07-22 14:30:13 +0530

    first

---
+++ regno.sh
@@ -0,0 +1,17 @@
+read regno;
+sum=0;
+#Length= expr Length #regno;
+num=0;
+for ((i=0;i<${#regno};i++))
+do
+var="${regno:$i:1}"
+if [[ "$var" =~ ^[0-9]+$ ]]
+then num=$(($num*10+$var))
+fi
+done
+for ((i=1;i<${#num};i=i+2))
+do 
+#echo "${num:$i:1}"
+sum=$(($sum+${num:$i:1}))
+done
+echo $sum;
